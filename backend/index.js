a685c38bd0f8554cc99143081bfbf430c8a8714b
Omair Hashmi <ohashmi21@gmail.com>
2024-09-26 19:47:37 -0400

    Setup firebase and wiki

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const fs = require("fs").promises; // Using fs.promises for asynchronous file operations
-const cors = require("cors");
-const app = express();
-
-app.use(express.json());
-app.use(cors());
