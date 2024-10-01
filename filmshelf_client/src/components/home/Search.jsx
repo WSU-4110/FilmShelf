0df6af160c741b9f34e1ed144ab2b20a4da283e8
Mikolaj Sobiesiuk <mikosob176545@gmail.com>
2024-10-01 17:11:12 -0400

    work done search bar

---
+++ filmshelf_client/src/components/home/Search.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import React from "react";
+import "./Search.css";
+
+function SearchBar() {
+    return (
+        <div classname="Search">
+            <div classname="Search-Bar-Container">
+                <div>SearchBar</div>
+                <div>SearchResults</div>
+            </div>
+        </div>
+    );
+}
