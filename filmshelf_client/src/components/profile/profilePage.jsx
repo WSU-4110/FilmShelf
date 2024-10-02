1da74d6e972d70af39a08e4cb048dfdb8d3ae5ca
Syed Shahzaib Azmat <shahzaibazmat4star@gmail.com>
2024-10-02 19:43:18 -0400

    Added image to profile card and profile settings button

--- filmshelf_client/src/components/profile/profilePage.jsx
+++ filmshelf_client/src/components/profile/profilePage.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { NavBar } from "../nav/nav";;
 import { auth } from '../../config/firebase-config'; 
 import { useState, useEffect } from 'react';
-import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBBtn, MDBRipple, MDBCardImage } from 'mdb-react-ui-kit';
 
 
 function ProfilePage() {
     const [user, setUser] = useState(null);
 
     useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+        const unsubscribe = auth.onAuthStateChanged((currentUser) => {      
             setUser(currentUser);
         });
 
@@ -22,17 +22,28 @@ function ProfilePage() {
             {user ? (
                 <div className="vh-100">
                     <MDBContainer>
+                        
                         <MDBRow className="justify-content-center">
                             <MDBCol md="9" lg="7" xl="5" className="mt-5">
                                 <MDBCard style={{ borderRadius: '15px' }}>
+                                        <MDBCardImage src='https://mdbootstrap.com/img/new/standard/nature/111.webp'/>
                                     <MDBCardBody className="p-4">
                                         <div className="d-flex text-black">
-                                            <div className="flex-shrink-0">
+                                            <div className= "flex-shrink-0">   
                                             </div>
                                             <div className="flex-grow-1 ms-3">
                                                 <MDBCardTitle>{user.displayName}</MDBCardTitle>
-                                                <MDBCardText>{user.email}</MDBCardText>
+                                                <MDBCardText>{user.email}</MDBCardText> 
 
+                                                    <div>
+                                                        <a href="#"><button>Profile Settings</button></a>
+                                                        </div>
+
+                                                        <br />
+
+                                                        <MDBBtn>Profile Settings</MDBBtn>
+
+                                                <br />
                                                 <div className="d-flex justify-content-start rounded-3 p-2 mb-2" style={{ backgroundColor: '#efefef' }}>
                                                     <div>
                                                         <p className="small text-muted mb-1">Movies Watched</p>
@@ -54,6 +65,8 @@ function ProfilePage() {
                             </MDBCol>
                         </MDBRow>
                     </MDBContainer>
+
+                    
                 </div>
             ) : (
                 <MDBContainer>
@@ -68,8 +81,12 @@ function ProfilePage() {
                     </MDBRow>
                 </MDBContainer>
             )}
+
+        
         </>
     );
 }
+
+
 
 export default ProfilePage;
