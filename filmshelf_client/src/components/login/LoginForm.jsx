2aebead3fdbae64d2051818ab10edeb1ac6a531a
Ibrahim Ivan <hl2635@wayne.edu>
2024-10-03 07:10:12 -0400

    added login using email and Password.

--- filmshelf_client/src/components/login/LoginForm.jsx
+++ filmshelf_client/src/components/login/LoginForm.jsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./loginForm.css";
 import { auth } from "../../config/firebase-config";
-import { signInWithGoogle } from "../auth";
+import { signInWithGoogle, doSignInWithEmailAndPassword } from "../auth";
 import { NavBar } from "../nav/nav";
 import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   const [user, setUser] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await doSignInWithEmailAndPassword(email, password);
+  };
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((currentUser) => {
@@ -32,8 +41,35 @@ const LoginForm = () => {
       <NavBar />
       <div className="loginContainer">
         <div className="wrapper">
+          <input
+            type="text"
+            autoComplete="email"
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="email"
+            className="email-input"
+          />
+          <input
+            type="text"
+            autoComplete="email"
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="password"
+            className="password-input"
+          />
+          <button className="submit" onClick={onSubmit}>
+            Sign in
+          </button>
           <h1 className="title">Login using Google</h1>
-          <button onClick={signInWithGoogle}>Google Sign In</button>
+          <button className="sign-in" onClick={signInWithGoogle}>
+            Google Sign In
+          </button>
         </div>
       </div>
     </div>
