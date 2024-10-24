b46b2ca37ae1a45576fb6ad155be6b6ba3bf88f5
Syed Shahzaib Azmat <shahzaibazmat4star@gmail.com>
2024-10-23 21:39:21 -0400

    changed button design, experimented with changing profile image

--- filmshelf_client/src/components/profile/profilePage.jsx
+++ filmshelf_client/src/components/profile/profilePage.jsx
@@ -4,6 +4,7 @@ import { auth } from '../../config/firebase-config';
 import { useState, useEffect } from 'react';
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBBtn, MDBRipple, MDBCardImage } from 'mdb-react-ui-kit';
 import "./profilePage.css";
+import profilePic from '../../assets/profileImage.jpg'
 
 function ProfilePage() {
     const [user, setUser] = useState(null);
@@ -21,55 +22,59 @@ function ProfilePage() {
             <NavBar />
             {user ? (
                 <div className="vh-100">
-                    <div className='profilePageContainer'>
+                <div className='profilePageContainer'>
 
-                    <MDBContainer>
-                        
-                        <MDBRow className="justify-content-center">
-                            <MDBCol md="9" lg="7" xl="5" className="mt-5">
-                                <MDBCard style={{ borderRadius: '15px', borderWidth: '0', backgroundColor: 'lightsteelblue'}}>
-                                        <MDBCardImage style={{borderRadius: '15px 15px 0 0'}}src='https://mdbootstrap.com/img/new/standard/nature/111.webp'/>
-                                    <MDBCardBody className="p-4">
-                                        <div className="d-flex text-black">
-                                            <div className= "flex-shrink-0">   
-                                            </div>
-                                            <div className="flex-grow-1 ms-3">
-                                                <MDBCardTitle>{user.displayName}</MDBCardTitle>
-                                                <MDBCardText>{user.email}</MDBCardText> 
+                <MDBContainer>
+                    
+                    <MDBRow className="justify-content-center">
+                        <MDBCol md="9" lg="7" xl="5" className="mt-5">
+                            <MDBCard style={{ borderRadius: '15px', borderWidth: '0', backgroundColor: 'lightsteelblue'}}>
+                                    <MDBCardImage style={{borderRadius: '15px 15px 0 0'}}src='https://mdbootstrap.com/img/new/standard/nature/111.webp'/>
+                                <MDBCardBody className="p-4">
+                                    <div className="d-flex text-black">
+                                        <div className= "flex-shrink-0">   
+                                        </div>
+                                        <div className="flex-grow-1 ms-3">
+                                            <MDBCardTitle>{user.displayName}</MDBCardTitle>
+                                            <MDBCardText>{user.email}</MDBCardText> 
+                                                
+                                                
+                                                <div className='buttons'>
+                                                    <button className='button'>Profile Settings</button>
+                                                </div>
+                                                
+                                                <br />
+                                                
+                                                
+                                                   
+
                                                     
-                                                    <div>
-                                                        <a>
-                                                            <button>Profile Settings</button>
-                                                            </a>
-                                                        </div>
 
-                                                        <br />
-
-                                                <div className="d-flex justify-content-start rounded-3 p-2 mb-2" style={{ backgroundColor: '#efefef' }}>
-                                                    <div>
-                                                        <p className="small text-muted mb-1">Movies Watched</p>
-                                                        <p className="mb-0">41</p>
-                                                    </div>
-                                                    <div className="px-3">
-                                                        <p className="small text-muted mb-1">Reviews</p>
-                                                        <p className="mb-0">976</p>
-                                                    </div>
-                                                    <div>
-                                                        <p className="small text-muted mb-1">Followers</p>
-                                                        <p className="mb-0">8.5</p>
-                                                    </div>
+                                            <div className="d-flex justify-content-start rounded-3 p-2 mb-2" style={{ backgroundColor: '#efefef' }}>
+                                                <div>
+                                                    <p className="small text-muted mb-1">Movies Watched</p>
+                                                    <p className="mb-0">41</p>
+                                                </div>
+                                                <div className="px-3">
+                                                    <p className="small text-muted mb-1">Reviews</p>
+                                                    <p className="mb-0">976</p>
+                                                </div>
+                                                <div>
+                                                    <p className="small text-muted mb-1">Followers</p>
+                                                    <p className="mb-0">8.5</p>
                                                 </div>
                                             </div>
                                         </div>
-                                    </MDBCardBody>
-                                </MDBCard>
-                            </MDBCol>
-                        </MDBRow>
-                    </MDBContainer>
+                                    </div>
+                                </MDBCardBody>
+                            </MDBCard>
+                        </MDBCol>
+                    </MDBRow>
+                </MDBContainer>
 
-                
-                    </div>
+            
                 </div>
+            </div>
             ) : (
                 <MDBContainer>
                     <MDBRow className="justify-content-center">
