fc1f546ae51f2ef2ca909bfa78f65688283bcfff
Syed Shahzaib Azmat <shahzaibazmat4star@gmail.com>
2024-10-31 17:40:26 -0400

    Implemented Factory Method Design Pattern

--- filmshelf_client/src/components/profile/profileSettings.jsx
+++ filmshelf_client/src/components/profile/profileSettings.jsx
@@ -18,6 +18,32 @@ function ProfileSettings() {
         return () => unsubscribe();
     }, []);
 
+    const buttonFactory = (type) => {
+        switch(type) {
+            case 'ChangeAccount':
+                return (
+                    <button className='chngAcc'>
+                        <box-icon name='user-circle'></box-icon>
+                        <p>Change Account</p>
+                    </button>
+                );
+            case 'ChangePassword':
+                return (
+                    <button className='chngPass'>
+                        <box-icon name='key' ></box-icon>
+                        <p>Change Password</p>
+                    </button>
+                );
+            case 'ChangeProfileImage':
+                return (
+                    <button className='chngImg'>
+                        <box-icon name='image'></box-icon>
+                        <p>Change Profile</p>
+                    </button>
+                );
+        }
+    };
+
     return (
         <>
             <NavBar/>
@@ -25,9 +51,12 @@ function ProfileSettings() {
             <MDBContainer >
                 <MDBRow className="justify-content-center">
                     <MDBCol md="9" lg="7" xl="5" className="mt-5">
-                        <img style={{borderRadius: '50%', height:'200px', width:'200px', alignContent:'center'}} src={profilePic} className='img-fluid shadow-4' alt='...' />
-                        <p style={{color: "white"}}>{user?.displayName}</p>
-                        <p style={{color: "white"}}>{user?.email}</p>
+                        <img style={{borderRadius: '50%', height:'200px', width:'200px'}} src={profilePic} className='img-fluid shadow-4' alt='...' />
+                        <div className='userInfo'>
+                            <p style={{color: "white", textAlign: "center"}}>{user?.displayName}</p>
+                            <p style={{color: "white", textAlign: "center"}}>{user?.email}</p>
+                        </div>
+                        
                     </MDBCol>
                 </MDBRow>
             </MDBContainer>
@@ -38,18 +67,9 @@ function ProfileSettings() {
                         <MDBCard style={{ borderRadius: '15px', borderWidth: '0', backgroundColor: 'lightsteelblue'}}>
                             <MDBCardBody className='p-4'>
                                 <div className='profileSettingsButtons'>
-                                    <button className='chngAcc'>
-                                        <box-icon name='user-circle'></box-icon>
-                                        <p>Change Account</p>
-                                    </button>
-                                    <button className='chngPass'>
-                                        <box-icon name='key' ></box-icon>
-                                        <p>Change Password</p>
-                                    </button>
-                                    <button className='chngImg'>
-                                        <box-icon name='image'></box-icon>
-                                        <p>Change Profile</p>
-                                    </button>
+                                    {buttonFactory('ChangeAccount')}
+                                    {buttonFactory('ChangePassword')}
+                                    {buttonFactory('ChangeProfileImage')}
                                 </div>
                             </MDBCardBody>
                         </MDBCard>
