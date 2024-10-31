814356a73226cb62140874155c979ed76e87a956
Syed Shahzaib Azmat <shahzaibazmat4star@gmail.com>
2024-10-31 17:59:34 -0400

    Implemented Factory Method Design Pattern

--- filmshelf_client/src/components/profile/profileSettings.jsx
+++ filmshelf_client/src/components/profile/profileSettings.jsx
@@ -18,6 +18,32 @@ function ProfileSettings() {
         return () => unsubscribe();
     }, []);
 
+    const buttonFactory = (type) => {
+        switch(type) {
+            case 'ChangeAccount':
+                return (
+                    <button className='chngAcc'>
+                        <box-icon name='user-circle'></box-icon>
+                        <p>Change Account</p>
+                    </button>
+                );
+            case 'ChangePassword':
+                return (
+                    <button className='chngPass'>
+                        <box-icon name='key' ></box-icon>
+                        <p>Change Password</p>
+                    </button>
+                );
+            case 'ChangeProfileImage':
+                return (
+                    <button className='chngImg'>
+                        <box-icon name='image'></box-icon>
+                        <p>Change Profile</p>
+                    </button>
+                );
+        }
+    };
+
     return (
         <>
             <NavBar/>
@@ -38,18 +64,9 @@ function ProfileSettings() {
                         <MDBCard style={{ borderRadius: '15px', borderWidth: '0', backgroundColor: 'lightsteelblue'}}>
                             <MDBCardBody className='p-4'>
                                 <div className='profileSettingsButtons'>
-                                    <button className='chngAcc'>
-                                        <box-icon name='user-circle'></box-icon>
-                                        <p>Change Account</p>
-                                    </button>
-                                    <button className='chngPass'>
-                                        <box-icon name='key' ></box-icon>
-                                        <p>Change Password</p>
-                                    </button>
-                                    <button className='chngImg'>
-                                        <box-icon name='image'></box-icon>
-                                        <p>Change Profile</p>
-                                    </button>
+                                    {buttonFactory('ChangeAccount')}
+                                    {buttonFactory('ChangePassword')}
+                                    {buttonFactory('ChangeProfileImage')}
                                 </div>
                             </MDBCardBody>
                         </MDBCard>
