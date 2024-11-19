fdf614087a8aa24403b441052adf9ebe33d81f21
eaziz7319 <ebaadaziz2004@gmail.com>
2024-11-19 13:48:46 -0500

    Perfromed unit testing on listPage.jsx functions to ensure it is working correctly.

--- filmshelf_client/vite.config.js
+++ filmshelf_client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,8 +9,13 @@ export default defineConfig({
       '/api/showtimes': {
         target: 'https://serpapi.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/showtimes/, '/search')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api\/showtimes/, '/search'),
+      },
+    },
+  },
+  test: {
+    environment: 'jsdom', // Use jsdom for DOM-like environment
+    globals: true, // Enable global test functions like `describe` and `it`
+    setupFiles: './vitest.setup.js', // (Optional) Path to setup file for mocking
+  },
+});
---
+++ filmshelf_client/vitest.setup.js
@@ -0,0 +1,1 @@
+import '@testing-library/jest-dom'; // Adds Jest DOM matchers
