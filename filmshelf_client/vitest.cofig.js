f9c3a6a55694debbe3eb5593ad0d18ea518e6968
Mikolaj Sobiesiuk <mikosob176545@gmail.com>
2024-11-19 23:33:55 -0500

    vitest

---
+++ filmshelf_client/vitest.cofig.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/showtimes': {
+        target: 'https://serpapi.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/showtimes/, '/search'),
+      },
+    },
+  },
+  test: {
+    environment: 'jsdom', // Use jsdom for DOM-like environment
+    globals: true, // Enable global test functions like `describe` and `it`
+    setupFiles: './vitest.setup.js', // (Optional) Path to setup file for mocking
+  },
+});
