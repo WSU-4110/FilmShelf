d3c88cb127a1b81c2edf3fabb59d4332687005d1
Ibrahim Ivan <hl2635@wayne.edu>
2024-12-03 19:06:13 -0500

    moved test folder inside of flimshelf client

--- filmshelf_client/src/Test/movieDetailsPage.test.jsx
+++ filmshelf_client/src/Test/movieDetailsPage.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
-import MoviesPage from "../MoviesPage";
+import MoviesPage from "../components/search/MoviesPage";
 import "@testing-library/jest-dom";
 import { vi } from "vitest";
 import { MemoryRouter } from "react-router-dom"; // Import MemoryRouter for routing context
